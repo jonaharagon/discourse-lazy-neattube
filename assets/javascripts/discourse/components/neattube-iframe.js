5bb34f9f19a253be41458a90cae06910f2e53de7
Jonah Aragon <jonah@triplebit.net>
2023-05-28 19:43:15 -0500

    Set iframe embed URL

--- assets/javascripts/discourse/components/neattube-iframe.js
+++ assets/javascripts/discourse/components/neattube-iframe.js
@@ -3,18 +3,12 @@ import Component from "@glimmer/component";
 export default class NeattubeVideo extends Component {
   get iframeSrc() {
     switch (this.args.providerName) {
-      case "youtube":
-        let url = `https://www.youtube.com/embed/${this.args.videoId}?autoplay=1`;
+      case "neattube":
+        let url = `https://neat.tube/videos/embed/${this.args.videoId}?autoplay=1`;
         if (this.args.startTime > 0) {
           url += `&start=${this.args.startTime}`;
         }
         return url;
-      case "vimeo":
-        return `https://player.vimeo.com/video/${this.args.videoId}${
-          this.args.videoId.includes("?") ? "&" : "?"
-        }autoplay=1`;
-      case "tiktok":
-        return `https://www.tiktok.com/embed/v2/${this.args.videoId}`;
     }
   }
 }

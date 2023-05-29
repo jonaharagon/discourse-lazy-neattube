750aa6a8b2a9c87525213677dacb45991bc2bffd
Jonah Aragon <jonah@triplebit.net>
2023-05-28 20:43:15 -0500

    CSS

--- plugin.rb
+++ plugin.rb
@@ -2,11 +2,13 @@
 
 # name: discourse-lazy-neattube
 # about: Lazy loading support for Neat.Tube videos
-# version: 0.1
+# version: 0.1.0
 # authors: Jonah Aragon, Jan Cernik
 # url: https://github.com/jonaharagon/discourse-lazy-neattube
 
 enabled_site_setting :lazy_neattube_enabled
+
+register_asset "stylesheets/neattube-videos.scss"
 
 require_relative "lib/neattube-videos/lazy_neattube"
 
